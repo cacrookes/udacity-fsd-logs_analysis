2427086b07fc7866bd5132f417117c05bd61107c
Christopher Crookes <cacrookes@gmail.com>
2017-07-22 23:19:04 -0700

    Cleaned up PEP8 warnings

--- logs_analysis.py
+++ logs_analysis.py
@@ -3,10 +3,10 @@
 Project: Logs Analysis, for Udacity Fullstack Nanodegree
 Author: Christopher Crookes
 
-log_analysis.py generates reports from the database set in DBNAME. Run directly to
-generate a report of the top 3 articles of all time by views, a ranking of the most
-popular authors by views, and a list of days were over 1% of access requests to the
-site were errors.
+log_analysis.py generates reports from the database set in DBNAME. Run directly
+to generate a report of the top 3 articles of all time by views, a ranking of
+the most popular authors by views, and a list of days were over 1% of access
+requests to the site were errors.
 
 This script can also be loaded as a module. Publically availabe functions:
 - execute_query
@@ -29,7 +29,7 @@ def __db_connect():
                     The second element is a cursor for the database.
     """
     try:
-        db = psycopg2.connect(dbname=DBNAME) 
+        db = psycopg2.connect(dbname=DBNAME)
         c = db.cursor()  # Create cursor
         return db, c
     except (Exception, psycopg2.DatabaseError) as error:
@@ -37,7 +37,7 @@ def __db_connect():
 
 
 def execute_query(query):
-    """execute_query takes an SQL query as a parameter. 
+    """execute_query takes an SQL query as a parameter.
         Executes the query and returns the results as a list of tuples.
        args:
            query - an SQL query statement to be executed.
@@ -68,7 +68,7 @@ def print_top_articles():
     print('-' * len(header))
     for title, views in results:
         print('\"{}\" -- {} views'.format(title, views))
-    
+
 
 def print_top_authors():
     """Prints a list of authors ranked by article views."""
@@ -77,26 +77,30 @@ def print_top_authors():
                GROUP BY author
                ORDER BY views DESC;"""
     results = execute_query(query)
-    
+
     header = 'Top Authors by All Time Views'
     print(header)
     print('-' * len(header))
     for title, views in results:
         print('{} -- {} views'.format(title, views))
 
+
 def print_error_days():
-    """Prints out the days where more than 1% of logged access requests were errors."""
+    """Prints out the days where more than 1% of
+    logged access requests were errors."""
+
     query = """SELECT date, (error_404::numeric / access_request) AS error_rate
                FROM daily_error_summary
                WHERE (error_404::numeric / access_request) > 0.01
                ORDER BY date;"""
     results = execute_query(query)
-    
+
     header = 'Days With Over 1% Error Access Rate'
     print(header)
     print('-' * len(header))
     for log_date, error_rate in results:
-        print('{0:%B %d, %Y} -- {1:.2f}% errors'.format(log_date, error_rate * 100))
+        print('{0:%B %d, %Y} -- {1:.2f}% errors'
+              .format(log_date, error_rate * 100))
 
 if __name__ == '__main__':
     print_top_articles()
@@ -104,4 +108,3 @@ if __name__ == '__main__':
     print_top_authors()
     print('\n')
     print_error_days()
-
